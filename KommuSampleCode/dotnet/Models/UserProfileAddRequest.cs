f6afd4eb5b62076a3d8af409d5cbcb1d7e9cabdc
Carlos Barrera <99574152+CarlosBarrera72@users.noreply.github.com>
2022-08-17 20:39:22 -0700

    Add files via upload

---
+++ KommuSampleCode/dotnet/Models/UserProfileAddRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests
+{
+    public class UserProfileAddRequest
+    {
+
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+        [StringLength(2, MinimumLength=0)]
+        public string Mi { get; set; }
+        [StringLength(128, MinimumLength = 2)]
+        public string AvatarUrl { get; set; }
+
+    }
+}
