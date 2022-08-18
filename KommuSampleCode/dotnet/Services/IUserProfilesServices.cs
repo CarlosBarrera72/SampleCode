ec1af8aff3d822641fc9e95bf327433deabf9bfa
Carlos Barrera <99574152+CarlosBarrera72@users.noreply.github.com>
2022-08-17 20:41:39 -0700

    Add files via upload

---
+++ KommuSampleCode/dotnet/Services/IUserProfilesServices.cs
@@ -0,0 +1,23 @@
+﻿using Sabio.Models;
+using Sabio.Models.Requests;
+using Sabio.Models.UserProfiles;
+
+namespace Sabio.Services.UserProfiles
+{
+    public interface IUserProfilesServices
+    {
+        UserProfile GetById(int id);
+
+        UserProfile GetCurrentUser(int id);
+
+        Paged<UserProfile> GetUserProfileAll(int pageIndex, int pageSize);
+
+        Paged<UserProfile> Search(int pageIndex, int pageSize, string query);
+
+        int AddProfile(UserProfileAddRequest model, int userId);
+
+        void UpdateProfile(UserProfileUpdateRequest model, int userId);
+
+        void DeleteProfile(int id, int userId);
+    }
+}
